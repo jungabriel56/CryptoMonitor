0ab1fc281aa297bb3f3b6d0085d89b4fea7861ad
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-09 20:19:00 -0300

    criando payload

---
+++ app/src/main/java/jungabriel56/com/github/cryptomonitor/MainActivity.kt
@@ -0,0 +1,25 @@
+package jungabriel56.com.github.cryptomonitor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val toolbarMain: Toolbar = findViewById(R.id.toolbar_main)
+        configureToolbar(toolbarMain)
+    }
+    private fun configureToolbar(toolbar: Toolbar) {
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        supportActionBar?.setTitle(getText(R.string.app_title))
+        supportActionBar?.setBackgroundDrawable(getDrawable(R.color.primary))
+
+    }
+
+
+}
